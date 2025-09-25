4c92536b3578c6bbc812c572ca7d26d262e16cbb
Jan Kühle <jkuehle90@gmail.com>
2025-09-25 11:36:09 +0200

    List on 404

--- src/elements/page-404.tsx
+++ src/elements/page-404.tsx
@@ -1,13 +1,21 @@
+import { ListDialogContent } from "./list-dialog";
 import { Toolbar } from "./toolbar";
+import { Dialog, useDialog } from "./utils/dialog";
 
 export function NotFoundPage() {
+    const [listDialogProps, openList] = useDialog();
     return (
         <>
-            <Toolbar title="Not found" />
+            <Toolbar title="Not found">
+                <button onClick={openList}>All</button>
+            </Toolbar>
             <p class="not-found">
                 Whooops. This page does not exist. I suggest you{" "}
                 <a href="/">start again</a>.
             </p>
+            <Dialog {...listDialogProps}>
+                <ListDialogContent />
+            </Dialog>
         </>
     );
 }
