91f6cfebd2056c52ab859f80f2522e67b5a996a1
Jan Kühle <jkuehle90@gmail.com>
2025-09-25 12:18:23 +0200

    Add close button to all dialog

--- src/elements/list-dialog.tsx
+++ src/elements/list-dialog.tsx
@@ -4,8 +4,13 @@ import { timeAgo } from "../shared/format.js";
 export function ListDialogContent() {
     return (
         <div class="notes-list">
-            <h1>Notes</h1>
-            <ul class="notes-list">
+            <header>
+                <h1>Notes</h1>
+                <form method="dialog">
+                    <input type="submit" value="Close" />
+                </form>
+            </header>
+            <ul>
                 {notes.value.map((note) => (
                     <li class="note-item" key={note.path}>
                         <a href={`/view/${note.path}`}>
